4f6c4ab9c4b15b9f5c1bc10f913b43da8a4d7c12
vhryhorovych <v.hryhorovych@aimprosoft.com>
2022-02-22 23:45:08 +0200

    Add components and routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { MyProfileComponent } from './components/my-profile/my-profile.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { CarsComponent } from './components/cars/cars.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent,
+  },
+  {
+    path: '/my-profile',
+    component: MyProfileComponent,
+  },
+  {
+    path: '/my-orders',
+    component: MyOrdersComponent,
+  },
+  {
+    path: '/cars',
+    component: CarsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,9 +7,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutComponent } from './components/layout/layout.component';
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { LandingComponent } from './components/landing/landing.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { MyProfileComponent } from './components/my-profile/my-profile.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
 
 @NgModule({
-  declarations: [AppComponent, LayoutComponent],
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    LandingComponent,
+    CarsComponent,
+    MyProfileComponent,
+    MyOrdersComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
